f10cc413730d62667a0f2a99270a9d3b499fb912
VanehSann <vanessacbsantos22@gmail.com>
2022-08-03 03:26:49 -0300

    resturando as func que comentei

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -2,14 +2,14 @@ const { Router } = require('express');
 
 const salesController = require('../controllers/salesController');
 
-// const validateSales = require('../middlewares/validateSales');
+const validateSales = require('../middlewares/validateSales');
 
 const router = Router();
 
 router.get('/', salesController.getAllSales);
-// router.post('/', validateSales, salesController.addNewSale);
+router.post('/', validateSales, salesController.addNewSale);
 router.get('/:id', salesController.getSaleById);
-// router.put('/:id', salesController.updateNewSale);
+router.put('/:id', salesController.updateNewSale);
 router.delete('/:id', salesController.deleteSale);
 
 module.exports = router;
